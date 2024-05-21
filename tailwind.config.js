54e383c71b53a4ea51ad0ccbe9462f5f64be8961
Pedro Herique <phpedrinh64@gmail.com>
2024-05-21 17:22:46 -0300

    ...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
-// tailwind.config.js
-module.exports = {
-  content: [
-    './pages/**/*.{html,js}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  // tailwind.config.js
+  module.exports = {
+    content: [
+      './**/*.{html,js}',
+    ],
+    theme: {
+      extend: {},
+      colors: {
+        gradient1: '#8149CF',
+        gradient2: '#8652CD',
+        gradient3: '#0E9EEF',
+        bgborder: '#6E15FF',
+        header: '#0E1920',
+        white: '#FFFFFF',
+        subtitle: '#BB8AFF',
+        background: '#252525',
+        playlist: '#0E9EEF',
+        login: '#1E1E1E',
+        signup: '#5F0BC9'
+      }
+    },
+    plugins: [],
+  }
